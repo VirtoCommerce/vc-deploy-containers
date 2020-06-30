187eabf008b26f6f83260a25615791ce6f38e0a3
Leo <leon@entechsolutions.com>
2020-06-30 17:55:01 +0400

    VP-3331: Add vc-platform-installer image building

--- vc-platform-installer/install-modules.py
+++ vc-platform-installer/install-modules.py
@@ -10,6 +10,10 @@ def getZipData(url):
     result = urllib.request.urlopen(url)
     return result.read()
 
+platformConfigUri = sys.argv[0]
+r = requests.get(platformConfigUri, allow_redirects=True)
+open('platform.json', 'wb').write(r.content)
+
 with open('platform.json') as f:
     config = json.load(f)
     for module in config["Modules"]:
