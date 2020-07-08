1352b7e0cdaca712a2a34f469da98a3339be767e
Maksim Kopnov <mko@virtoway.com>
2020-07-09 00:38:54 +0700

    VP-3454: Add restore-db script

---
+++ vc-platform-db-installer/restore-db.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+mkdir ./downloads
+wget -q "$1" -O ./downloads/db.bacpac
+/opt/mssql-tools/sqlpackage/sqlpackage /Action:Import /TargetServerName:"$2" /TargetUser:"$3" /TargetPassword:"$4" /TargetDatabaseName:"[$5]" /SourceFile:"./downloads/db.bacpac"
+rm -r ./downloads
