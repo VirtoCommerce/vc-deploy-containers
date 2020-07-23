f025e6a7a80ed04518050bc22f588e1507c3dfcc
Maksim Kopnov <44946644+mvktsk@users.noreply.github.com>
2020-07-24 01:16:59 +0700

    Update install-theme.sh

--- vc-platform-installer/install-theme.sh
+++ vc-platform-installer/install-theme.sh
@@ -3,6 +3,11 @@
 THEME_URL=$1
 THEME_LOCATION=$2
 
+if [ ! -d "$THEME_LOCATION" ]; then
+    # Создать папку, только если ее не было
+    mkdir $DIR
+fi
+
 wget -O theme.zip $THEME_URL
 unzip theme.zip
 cp -rp default/* $THEME_LOCATION
