95d59eebfccc9fc5dffb181c41975bab3f0b3975
Konstantin Kisilevskiy <xlonelysoul94@gmail.com>
2021-01-22 13:57:46 +0200

    VP-6712: Improve script for upload assets

--- vc-platform-installer/upload-assets.py
+++ vc-platform-installer/upload-assets.py
@@ -2,24 +2,57 @@ import sys
 import datetime
 import os
 import urllib.request 
-import tqdm
 import zipfile 
+import shutil
 
 inputzipfile = sys.argv[1]
 assetsFolder = sys.argv[2]
-checkFolder = (assetsFolder + '/assets')
+checkFolder = os.path.join(assetsFolder, 'assets')
+installed_assets_file = os.path.join(checkFolder, 'installed_assets.txt')
+
 from zipfile import ZipFile
-from tqdm import tqdm
-if(os.path.exists(checkFolder)):
-    print('Assets already exists')
-else:
-    print('Downloading assets...'); print(datetime.datetime.now())
+def download_assets():
+    print(datetime.datetime.now())
+    print('Downloading theme from:', inputzipfile)
     url = inputzipfile
     urllib.request.urlretrieve(url, "assets.zip")
-    print('Downloading assets successfuly !'); print(datetime.datetime.now()) 
-    print('Starting unarchiving!')
-    with ZipFile(file='assets.zip') as zip_file:
-        for file in tqdm(iterable=zip_file.namelist(), total=len(zip_file.namelist())):
-            zip_file.extract(member=file, path=assetsFolder)
-    print('Unarchiving successfuly !'); print(datetime.datetime.now())  
+    print(datetime.datetime.now())
+    print('Assets successfuly downloaded!')
+    with ZipFile('assets.zip', 'r') as zipObject:
+        listOfFileNames = zipObject.namelist()
+        for fileName in listOfFileNames:
+                zipObject.extract(fileName, assetsFolder)
+                print('Extraction...', fileName) 
+    installed_assets = open(installed_assets_file, 'w')
+    text_for_file = inputzipfile
+    installed_assets.write(text_for_file)
+    os.remove('assets.zip')
+
+def removing_old_assets(path):
+    if(os.path.exists(checkFolder)):
+        print(datetime.datetime.now())
+        print('Removing old assets...')
+        folder = path
+        for the_file in os.listdir(folder):
+            file_path = os.path.join(folder, the_file)
+            try:
+                if os.path.isfile(file_path):
+                    os.unlink(file_path)
+                elif os.path.isdir(file_path): shutil.rmtree(file_path)
+            except Exception as e:
+                print(e)
+        print(datetime.datetime.now())
+        print('Removing complete') 
+
+if(os.path.exists(installed_assets_file)):
+    check_assets_version = open(installed_assets_file, 'r')
+    current_version = check_assets_version.read()
+    if current_version != inputzipfile:
+        removing_old_assets(checkFolder)
+        download_assets() 
+    else:
+        print('Assets up to date')
+else:
+    removing_old_assets(checkFolder)
+    download_assets()
 
